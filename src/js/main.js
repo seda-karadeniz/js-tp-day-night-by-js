2a10cabc0232e3747281f7cfabfc6fed40980971
Daniel Schreurs <daniedaniel.schreurs@hotmail.com>
2019-11-27 17:31:59 +0100

    :sparkles: Création de la solution

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+const DayNight = {
+  cacheDom() {
+    this.body = document.body;
+    this.tumbler = document.querySelector('.tumbler');
+    this.posts = document.querySelectorAll('.post');
+    this.tumblerWrapper = document.querySelector('.tumbler__wrapper');
+  },
+  init() {
+    window.document.documentElement.classList.add('js-enabled');
+    this.cacheDom();
+    this.addEventListeners();
+  },
+  addEventListeners() {
+    console.log(this.tumblerWrapper);
+    this.tumblerWrapper.addEventListener('click', () => {
+      this.body.classList.toggle('body--night-mode');
+      this.tumbler.classList.toggle('tumbler--night-mode');
+      this.posts.forEach(post => {
+        post.classList.toggle('post--night-mode')
+      });
+    })
+  }
+
+};
+
+DayNight.init();
